080c85e0336c530a54e174f5eb3deaeab0cfe0d9
isaacaddis <isaacaddis21@gmail.com>
2018-11-10 21:10:54 -0800

    debug

--- seung.py
+++ seung.py
@@ -9,16 +9,21 @@ import pytesseract
 parser = argparse.ArgumentParser()
 parser.add_argument("-i","--image", required = True, help = "Input")
 parser.add_argument("-east","--east", type=str, required= True,help="path to EAST detector")
-parser.add_argument("-c","--c",type=str,help="Minimum confidence", default=0.5)
-parser.add_argument("-w", "--width",type=str, help="Width of resized image. Must be a multiple of 32 in order to work with EAST detector", default = 320)
-parser.add_argument("-e", "--height",type=str, help="Height of resized image. Must be a multiple of 32 in order to work with EAST detector", default = 320)
+#parser.add_argument("-c","--c",type=str,help="Minimum confidence", default=0.5)
+#parser.add_argument("-w", "--width",type=str, help="Width of resized image. Must be a multiple of 32 in order to work with EAST detector", default = 320)
+#parser.add_argument("-e", "--height",type=str, help="Height of resized image. Must be a multiple of 32 in order to work with EAST detector", default = 320)
 
 args = vars(parser.parse_args())
 
+'''
+    Image Operations
+    Let's clean up some input data
+'''
 image = cv2.imread(args["image"])
+(H, W) = image.shape[:2]
 image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-gray = cv2.GaussianBlur(gray,(5,5),5)
-
+image = cv2.GaussianBlur(image,(5,5),5)
+'''
 if args["width"] and args["height"] is not None:
     (newW, newH) = (args["width"], args["height"])
     rW = W/float(newW)
@@ -28,18 +33,20 @@ else:
     (newW, newH) = (320,320)
     rW = W/float(newW)
     rH = H/float(newH)
-    image = cv2.resize(image, (newW, newH))
+'''
+image = cv2.resize(image, (320, 320))
 layerNames = [
             "feature_fusion/Conv_7/Sigmoid",
                 "feature_fusion/concat_3"]
 print("[INFO] Loading EAST detector")
 text_dect = cv2.dnn.readNet(args["east"])
 startTime = time.time()
-text_dect.setInput(cv2.dnn.blobFromImage(image,1.0,(W,H), 123.68, 116.78, 103.94),swapRB=True, crop=False)
-(scores, geometry) = net.forward(layerNames)
-endTime = time.time()
-print("[INFO] Finished text detection in {::.6f} seconds".format(endTime-startTime))
-
+blob = cv2.dnn.blobFromImage(image, 1.0, (W, H),
+            (123.68, 116.78, 103.94), swapRB=True, crop=False)
+text_dect.setInput(blob)
+(scores, geometry) = text_dect.forward(layerNames)
+print("[INFO] Finished text detection in {::.6f} seconds".format(time.time()-startTime))
+'''
 (numRows, numCols) = scores.shape[2:4]
 rects = []
 confidences = []
@@ -51,4 +58,5 @@ for y in range(0, numRows):
     xData2 = geometry[0,2,y]
     xData3 = geometry[0,3,y]
     anglesData = geometry[0,4,y]
+'''
 
