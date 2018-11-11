1da76fe39c3fc70d3213cff69ecf006ea4c5851b
isaacaddis <isaacaddis21@gmail.com>
2018-11-11 11:53:53 -0800

    Cleanup

--- seung.py
+++ seung.py
@@ -20,21 +20,12 @@ args = vars(parser.parse_args())
     Let's clean up some input data
 '''
 image = cv2.imread(args["image"])
+orig = image.copy()
 (H, W) = image.shape[:2]
-image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-image = cv2.GaussianBlur(image,(5,5),5)
-'''
-if args["width"] and args["height"] is not None:
-    (newW, newH) = (args["width"], args["height"])
-    rW = W/float(newW)
-    rH = H/float(newH)
-    image = cv2.resize(image, (newW, newH))
-else:
-    (newW, newH) = (320,320)
-    rW = W/float(newW)
-    rH = H/float(newH)
-'''
+rW = W/320
+rH = H/320
 image = cv2.resize(image, (320, 320))
+(H, W) = image.shape[:2]
 layerNames = [
             "feature_fusion/Conv_7/Sigmoid",
                 "feature_fusion/concat_3"]
@@ -45,18 +36,42 @@ blob = cv2.dnn.blobFromImage(image, 1.0, (W, H),
             (123.68, 116.78, 103.94), swapRB=True, crop=False)
 text_dect.setInput(blob)
 (scores, geometry) = text_dect.forward(layerNames)
-print("[INFO] Finished text detection in {::.6f} seconds".format(time.time()-startTime))
-'''
-(numRows, numCols) = scores.shape[2:4]
+print("[INFO] Finished text detection in {:.6f} seconds".format(time.time()-startTime))
+
+(rows, cols) = scores.shape[2:4]
 rects = []
 confidences = []
 
-for y in range(0, numRows):
-    scoresData = scores[0,0,y]
-    xData0 = geometry[0,0,y]
-    xData1 = geometry[0,1,y]
-    xData2 = geometry[0,2,y]
-    xData3 = geometry[0,3,y]
-    anglesData = geometry[0,4,y]
-'''
+for y in range(0, rows):
+	scoresData = scores[0, 0, y]
+	xData0 = geometry[0, 0, y]
+	xData1 = geometry[0, 1, y]
+	xData2 = geometry[0, 2, y]
+	xData3 = geometry[0, 3, y]
+	anglesData = geometry[0, 4, y]
+
+	for x in range(0, cols):
+		if scoresData[x] < .65:
+			continue
+		(offsetX, offsetY) = (x * 4.0, y * 4.0)
+		angle = anglesData[x]
+		cos = np.cos(angle)
+		sin = np.sin(angle)
+		h = xData0[x] + xData2[x]
+		w = xData1[x] + xData3[x]
+		endX = int(offsetX + (cos * xData1[x]) + (sin * xData2[x]))
+		endY = int(offsetY - (sin * xData1[x]) + (cos * xData2[x]))
+		startX = int(endX - w)
+		startY = int(endY - h)
+		rects.append((startX, startY, endX, endY))
+		confidences.append(scoresData[x])
+boxes = non_max_suppression(np.array(rects), probs=confidences)
+for (startX, startY, endX, endY) in boxes:
+	startX = int(startX * rW)
+	startY = int(startY * rH)
+	endX = int(endX * rW)
+	endY = int(endY * rH)
+	cv2.rectangle(orig, (startX, startY), (endX, endY), (0, 255, 0), 2)
+cv2.imshow("Text Detect",orig)
+cv2.waitKey(0)
 
